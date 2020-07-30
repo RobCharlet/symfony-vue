8812c545db3952cb6c1ad73a889cc317691f6720
Robin Charlet <robin.charlet@laposte.net>
2020-07-30 14:26:40 +0200

    Create Legend component

---
+++ assets/js/components/legend.vue
@@ -0,0 +1,15 @@
+<template>
+    <span class="p-3">
+        {{ legend }}
+    </span>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            legend: "Shipping takes 10-12 weeks, and products probably won't work",
+        };
+    },
+};
+</script>
