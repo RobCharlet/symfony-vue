e372b26bcf77715598b8353530a704913c2c5970
Robin Charlet <robin.charlet@laposte.net>
2020-08-03 16:03:57 +0200

    Pass currentCategoryId server data (from SYmfony) to Vue

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,10 +22,16 @@ class ProductController extends AbstractController
 
     /**
      * @Route("/category/{id}", name="app_category")
+     * @param Category              $category
+     * @param IriConverterInterface $iriConverter
+     *
+     * @return Response
      */
-    public function showCategory(Category $category): Response
+    public function showCategory(Category $category, IriConverterInterface $iriConverter): Response
     {
-        return $this->render('product/index.html.twig');
+        return $this->render('product/index.html.twig', [
+            'currentCategoryId' => $iriConverter->getIriFromItem($category),
+        ]);
     }
 
     /**
