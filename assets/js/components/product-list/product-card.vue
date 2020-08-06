1afd6410052e659d0ebb3b2b243de004713c55ab
Robin Charlet <robin.charlet@laposte.net>
2020-08-06 15:47:07 +0200

    Create formatPrice helper

--- assets/js/components/product-list/product-card.vue
+++ assets/js/components/product-list/product-card.vue
@@ -30,6 +30,8 @@
 </template>
 
 <script>
+import formatPrice from '@/helper/format-price';
+
 export default {
     name: 'ProductCard',
     props: {
@@ -45,7 +47,7 @@ export default {
          * @returns {string}
          */
         price() {
-            return (this.product.price / 100).toLocaleString('en-US', {minimumFractionDigits: 2});
+            return formatPrice(this.product.price);
         },
     },
 };
