85304424944e748f4f1fc6028efd2caff5eb93a0
Robin Charlet <robin.charlet@laposte.net>
2020-08-06 11:27:08 +0200

    Create searchbar

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         <div class="row">
-            <div class="col-12">
+            <div class="col-3">
                 <title-component
                     :categories="categories"
                     :current-category-id="currentCategoryId"
                 />
+            </div>
+            <div class="col-9">
+                <search-bar />
             </div>
         </div>
         <product-list
@@ -23,12 +26,14 @@ import LegendComponent from '@/components/legend';
 import ProductList from '@/components/product-list';
 import { fetchProducts } from '@/services/products-service';
 import TitleComponent from '@/components/title';
+import SearchBar from '@/components/search-bar';
 
 export default {
     name: 'Catalog',
     components: {
         LegendComponent,
         ProductList,
+        SearchBar,
         TitleComponent,
     },
     props: {
