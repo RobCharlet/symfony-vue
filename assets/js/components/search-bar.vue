b7f629e0f9fd346024e227632457f31e977e836a
Robin Charlet <robin.charlet@laposte.net>
2020-08-07 11:29:18 +0200

    Add custom clear search

--- assets/js/components/search-bar.vue
+++ assets/js/components/search-bar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="input-group">
         <input
             v-model="searchTerm"
             class="form-control"
@@ -7,6 +7,16 @@
             type="search"
             @input="onInput"
         />
+
+        <div
+            v-show="searchTerm !== ''"
+            class="input-group-append"
+            @click="eraseSearchTerm"
+        >
+            <button class="btn btn-outline-secondary">
+                X
+            </button>
+        </div>
     </div>
 </template>
 
@@ -20,6 +30,10 @@ export default {
         };
     },
     methods: {
+        eraseSearchTerm() {
+            this.searchTerm = '';
+            this.$emit('search-products', { term: this.searchTerm });
+        },
         onInput() {
             // Debounce Reset timer if already exists
             if (this.searchTimeout) {
